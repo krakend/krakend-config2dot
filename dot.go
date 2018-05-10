9b80f11dfca32ada6b81d4f4808f3288195266ae
Albert <alombarte@gmail.com>
2018-05-10 20:14:49 +0200

    Body paint

--- dot.go
+++ dot.go
@@ -24,34 +24,38 @@ func WriteDot(w io.Writer, cfg config.ServiceConfig) (n int64, err error) {
 }
 
 const tmplGraph = `digraph krakend { {{ $port := .Port }}
-    label="KrakenD Gateway";
-    labeljust="l";
-    fontname="Ubuntu";
-    fontsize="13";
-    rankdir="LR";
-    bgcolor="aliceblue";
-    style="solid";
-    penwidth="0.5";
-    pad="0.0";
-    nodesep="0.35";
+    label="Server configuration - KrakenD API Gateway";
+		fontsize="25";
+		fontcolor="midnightblue";
+		labelloc="t";
+		labeljust="l";
+		rankdir="LR";
+		bgcolor="transparent";
+		style="solid";
+		penwidth="0.5";
+		pad="0.0";
+		nodesep="0.35";
 
-    node [shape="ellipse" style="filled" fillcolor="honeydew" fontname="Ubuntu" penwidth="1.0" margin="0.05,0.0"];
+		node [shape="circle" style="filled"  fillcolor="#e0ecf4" color="lightgray" fontsize="12"];
 
 	{{ range $i, $endpoint := .Endpoints }}
     {{printf "subgraph \"cluster_%s\" {" .Endpoint }}
-    	label="{{ .Endpoint }}";
-    	bgcolor="lightgray";
-    	shape="box";
-    	style="solid";
+		label="{{ .Endpoint }}";
+		bgcolor="#f7fcfd";
+		color="gainsboro";
+		shape="box";
+		style="solid";
+		fontsize="16";
 
         "{{ .Endpoint }}" [ shape=record, label="{ { Timeout | {{.Timeout.String}} } | { CacheTTL | {{.CacheTTL.String}} } | { Output | {{.OutputEncoding}} } | { QueryString | {{.QueryString}} } }" ]
         {{ if .ExtraConfig }}"extra_{{$i}}" [ shape=record, label="{ {ExtraConfig} {{ range $key, $value := .ExtraConfig }} | { {{ $key }} {{ range $k, $v := $value }}| { {{$k}} | {{$v}} } {{ end }} }{{ end }} }" ]{{ end }}
 	    {{ range $j, $backend := .Backend }}
 	    {{printf "subgraph \"cluster_%s\" {" .URLPattern }}
-	    	label="{{ .URLPattern }}";
-	    	bgcolor="beige";
-	    	shape="box";
-	    	style="solid";
+			label="{{ .URLPattern }}";
+			bgcolor="#e0ecf4";
+			fontsize="16";
+			shape="box";
+			style="solid";
         	"in_{{$i}}_{{$j}}" [ shape=record, label="{ {sd|{{ if .SD }}{{ .SD }}{{ else }}static{{ end }} } | { Hosts | {{.Host}} } | { Encoding | {{ if .Encoding }}{{ .Encoding }}{{ else }}JSON{{ end }} } }" ]
         {{ if .ExtraConfig }}"extra_{{$i}}_{{$j}}" [ shape=record, label="{ { ExtraConfig {{ range $key, $v := .ExtraConfig }} | {{ $key }} {{ end }} } }" ]{{ end }}
 	    {{println "}" }}
